a067b51dda87644219326e6fa8b899a16c0cc476
ritiek <ritiekmalhotra123@gmail.com>
2017-08-06 20:04:20 +0530

    Extract filename automatically

--- tudl.py
+++ tudl.py
@@ -10,17 +10,14 @@ import argparse
 def argparser():
     parser = argparse.ArgumentParser(
         description='Download files from tinyupload.com')
-    parser.add_argument(
-        'query',
-        metavar='QUERY',
-        type=str,
-        nargs='*',
-        help='the file_id or the tinyupload link')
     required = parser.add_argument_group('required arguments')
+
     required.add_argument(
-        '-n', '--name',
-        help='path and name of the file',
-        required=True)
+        'query',
+        help='the file_id or the tinyupload link')
+    required.add_argument(
+        '-o', '--output-file',
+        help='path and name of the file')
 
     return parser
 
@@ -28,7 +25,7 @@ def argparser():
 def get_file_id(link):
     if not len(link) == 20 and 'tinyupload.com' in link:
         link = link.rstrip('/')
-    return link[:-20]
+    return link[-20:]
 
 
 def make_soup(file_id):
@@ -37,16 +34,26 @@ def make_soup(file_id):
     return soup
 
 
-def get_link(soup):
+def get_filelink(soup):
     for x in soup.find_all('a'):
         if x['href'].startswith('download.php'):
             full_link = 'http://s000.tinyupload.com/' + x['href']
             return full_link
 
 
-def download(link, name):
-    download_file = requests.get(full_link, stream=True)
-    with open(name, 'wb') as handle:
+def get_filename(soup):
+    name = soup.find_all('b')[-2].get_text()
+    return name
+
+
+def get_filesize(soup):
+    # in progress
+    return
+
+
+def download(link, output):
+    download_file = requests.get(link, stream=True)
+    with open(output, 'wb') as handle:
         for block in download_file.iter_content(1024):
             handle.write(block)
 
@@ -57,12 +64,17 @@ def command_line():
     args = parser.parse_args(raw_args)
 
     query = args.query
-    name = args.name
+    output = args.output_file
 
     file_id = get_file_id(query)
     soup = make_soup(file_id)
-    link = get_link(soup)
-    download(link, name)
+    link = get_filelink(soup)
+
+    if not output:
+        output = get_filename(soup)
+
+    download(link, output)
+
 
 
 if __name__ == '__main__':
