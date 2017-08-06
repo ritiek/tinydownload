7422eadbc0b238e1fbd9d2fd328125f6091491dc
Ritiek <ritiekmalhotra123@gmail.com>
2017-08-06 14:59:01 +0530

    Add README and some minor fixes

---
+++ README.rst
@@ -0,0 +1,51 @@
+tinyupload-downloader =====================
+
+|pypi.python.org| |build Status|
+
+Download files from tinyupload.com.
+
+Installation & Usage:
+---------------------
+
+::
+
+    sudo pip install -U tinyupload-downloader
+
+or if you like to live on the bleeding edge:
+
+::
+
+    sudo python setup.py install
+
+Run it using ``tudl``
+
+::
+
+    usage: tudl.py [-h] -n NAME [QUERY [QUERY ...]]
+
+    Download files from tinyupload.com
+
+    positional arguments:
+      QUERY                 the file_id or the tinyupload link
+
+    optional arguments:
+      -h, --help            show this help message and exit
+
+    required arguments:
+      -n NAME, --name NAME  path and name of the file
+
+Contributing:
+-------------
+
+Any PR's in bug fixes, features or even this documentation are most
+welcome!
+
+License:
+--------
+
+``The MIT License``
+
+.. |pypi.python.org| image:: https://img.shields.io/pypi/v/tinyupload-downloader.svg
+   :target: https://pypi.org/project/tinyupload-downloader/
+.. |build Status| image:: https://travis-ci.org/ritiek/tinyupload-downloader.svg?branch=master
+   :target: https://travis-ci.org/ritiek/tinyupload-downloader/
--- tudl.py
+++ tudl.py
@@ -6,7 +6,8 @@ import os
 import sys
 import argparse
 
-def parse_args():
+
+def argparser():
     parser = argparse.ArgumentParser(
         description='Download files from tinyupload.com')
     parser.add_argument(
@@ -15,21 +16,26 @@ def parse_args():
         type=str,
         nargs='*',
         help='the file_id or the tinyupload link')
-    parser.add_argument(
+    required = parser.add_argument_group('required arguments')
+    required.add_argument(
         '-n', '--name',
-        required=True,
-        help='path and name of the file')
+        help='path and name of the file',
+        required=True)
+
     return parser
+
 
 def get_file_id(link):
     if not len(link) == 20 and 'tinyupload.com' in link:
         link = link.rstrip('/')
     return link[:-20]
 
+
 def make_soup(file_id):
     response = requests.get('http://s000.tinyupload.com/?file_id=' + file_id)
     soup = BeautifulSoup(response.text, 'html.parser')
     return soup
+
 
 def get_link(soup):
     for x in soup.find_all('a'):
@@ -37,21 +43,28 @@ def get_link(soup):
             full_link = 'http://s000.tinyupload.com/' + x['href']
             return full_link
 
+
 def download(link, name):
     download_file = requests.get(full_link, stream=True)
     with open(name, 'wb') as handle:
         for block in download_file.iter_content(1024):
             handle.write(block)
 
+
 def command_line():
+    parser = argparser()
     raw_args = sys.argv[1:]
     args = parser.parse_args(raw_args)
+
     query = args.query
-    name = query.name
+    name = args.name
+
     file_id = get_file_id(query)
     soup = make_soup(file_id)
     link = get_link(soup)
     download(link, name)
 
+
 if __name__ == '__main__':
+
     command_line()
